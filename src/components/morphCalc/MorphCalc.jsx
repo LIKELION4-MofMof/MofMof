a71ed119392a46cffa1947b525c890cd9fbc65fa
Juhee Noh <nohjuhee227@gmail.com>
2023-03-16 13:14:23 +0900

    [styles] 모프계산기 페이지 좌우 패딩 적용 #5

--- src/components/morphCalc/MorphCalc.jsx
+++ src/components/morphCalc/MorphCalc.jsx
@@ -1,74 +1,16 @@
-import React, { useState } from 'react';
-import MorphButtonList from './morphInputContainer/MorphButtonList';
-import MorphInput from './morphInputContainer/MorphInput';
+import styled from 'styled-components';
 import MorphInputContainer from './morphInputContainer/MorphInputContainer';
 
-const fattailMorph = [
-  {
-    id: 'fm1',
-    engName: 'Normal',
-    korName: '노말',
-    type: 'dominant',
-  },
-  {
-    id: 'fm2',
-    engName: 'Oreo',
-    korName: '오레오',
-    type: 'recessive',
-  },
-  {
-    id: 'fm3',
-    engName: 'Whiteout',
-    korName: '화이트아웃',
-    type: 'dominant',
-  },
-  {
-    id: 'fm4',
-    engName: 'Stripe',
-    korName: '스트라이프',
-    type: 'dominant',
-  },
-  {
-    id: 'fm5',
-    engName: 'Zero',
-    korName: '제로',
-    type: 'recessive',
-  },
-  {
-    id: 'fm6',
-    engName: 'Caramel',
-    korName: '카라멜',
-    type: 'recessive',
-  },
-  {
-    id: 'fm7',
-    engName: 'Ghost',
-    korName: '고스트',
-    type: 'recessive',
-  },
-];
+const DivMorphCalc = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+`;
 
 const MorphCalc = () => {
-  const [morphList, setMorphList] = useState([]);
-
-  const showDropDownMorphList = () => {
-    setMorphList(fattailMorph);
-  };
-  const filterDropDownMorphList = (filterMorph) => {
-    setMorphList(filterMorph);
-  };
-
   return (
-    <div>
-      <MorphInputContainer title={'아빠'}>
-        <MorphInput
-          showDropDownMorphList={showDropDownMorphList}
-          fattailMorph={fattailMorph}
-          filterDropDownMorphList={filterDropDownMorphList}
-        />
-        <MorphButtonList morphList={morphList} />
-      </MorphInputContainer>
-    </div>
+    <DivMorphCalc>
+      <MorphInputContainer title={'아빠'}></MorphInputContainer>
+    </DivMorphCalc>
   );
 };
 
