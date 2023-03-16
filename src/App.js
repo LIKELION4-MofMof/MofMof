edfc391824559ba4fb2dd980aae7d0a68f886a37
hwany <hwanyplaying@gmail.com>
2023-03-16 17:13:53 +0900

    [style] 모프 소개 페이지 마크업 ì 완료

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Layout from './components/common/Layout/Layout';
-import FattailHome from "./pages/FattailHome/FattailHome";
-import CrestedHome from './pages/CrestedHome/CrestedHome';
-import LeopardHome from './pages/LeopardHome/LeopardHome';
+import Fattail from './components/main/Fattail';
+import Crested from './components/main/Crested';
+import Leopard from './components/main/Leopard';
+
 
 function App() {
   return (
     <div className="App">
       <Layout />
-      <button>Fattail</button>
-      <button>Crested</button>
-      <button>Leopard</button>
-        <Router>
-          <Routes>
-            <Route path="/fattail-home" element={<FattailHome />} />
-            <Route path="/crested-home" element={<CrestedHome />} />
-            <Route path="/leopard-home" element={<LeopardHome />} />
-          </Routes>
-        </Router>
+
+      <Fattail />
+
+      <Crested />
+
+      <Leopard />
     </div>
   )
 }
