f3a0300c104993b21a5eb5c71f0ede2675c3aee9
Juhee Noh <nohjuhee227@gmail.com>
2023-03-17 10:11:31 +0900

    [styles] 모프계산기 페이지 레이아웃 #16

--- src/components/morphCalc/MorphCalc.jsx
+++ src/components/morphCalc/MorphCalc.jsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 import MorphInputContainer from './morphInputContainer/MorphInputContainer';
 
 const DivMorphCalc = styled.div`
+  width: 375px;
+  border: 1px solid black;
+  border-radius: 10px;
+  height: 100vh;
+  padding-top: 30px;
   padding-left: 20px;
   padding-right: 20px;
 `;
