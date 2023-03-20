5fcc63ec7003d0c9e29eafc8782ad432139998e8
hamjongseok <irang4605@naver.com>
2023-03-20 13:40:01 +0900

    [Feature] geneDNA 유전자합성 함수 구현

--- src/components/morphCalc/CalculateFattail.jsx
+++ src/components/morphCalc/CalculateFattail.jsx
@@ -1,5 +1,56 @@
 import { convertKorToEng, checkName } from './CalcFunc';
 
+//모프 객체에 맞는 유전자 합성
+function geneDNA(parent1, parent2, geneList1, geneList2) {
+  let parentName1 = [];
+
+  parent1 = geneList1
+    .map((item, idx) => {
+      parentName1.push(item.engName); //모프이름 들어감
+      const checkUpperLower = item.alphaCode;
+      const checkChar = item.character;
+
+      if (checkUpperLower === checkUpperLower.toUpperCase()) {
+        return checkUpperLower + 'N';
+      } else {
+        if (checkChar === 'Het') {
+          return checkUpperLower + 'n';
+        } else {
+          return checkUpperLower + checkUpperLower;
+        }
+      }
+    })
+    .toString(); //[aa]를 aa로 문자열로 만들어줌
+  parent1 = parent1.replace(/,/g, '');
+  console.log('페런츠1입니다', parent1);
+
+  let parentsName2 = [];
+  parent2 = geneList2
+    .map((item, index) => {
+      parentsName2.push(item.engName);
+
+      const checkUpperLower = item.alphaCode;
+      const checkChar = item.character;
+
+      if (checkUpperLower === checkUpperLower.toUpperCase()) {
+        return checkUpperLower + 'N';
+      } else {
+        if (checkChar === 'Het') {
+          return checkUpperLower + 'n';
+        } else {
+          return checkUpperLower + checkUpperLower;
+        }
+      }
+    })
+    .toString();
+  parent2 = parent2.replace(/,/g, '');
+  // console.log('페런츠2입니다', parent2);
+  // console.log('ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ?', parentName1);
+  // console.log(parentsName2);
+
+  return parent1, parent2;
+}
+
 export const calculate = (gene1, gene2, setResult) => {
   //예외처리
   if (gene1.length === 0 || gene2.length === 0) {
@@ -29,4 +80,17 @@ export const calculate = (gene1, gene2, setResult) => {
   geneList2 = checkName(geneEng2, alphaValues);
 
   console.log('유전자에 맞게 값 할당 ', geneList1, geneList2);
+
+  //부모리스트 끼리 서로 검사하여 중복값이 있다면 같은 알파벳으로 설정해줌
+  geneList2.forEach((gene2) => {
+    const matchingGene = geneList1.find(
+      (gene1) => gene1.engName === gene2.engName,
+    );
+    if (matchingGene) {
+      gene2.alphaCode = matchingGene.alphaCode;
+    }
+  });
+
+  //3번째 알고리즘
+  geneDNA(parent1, parent2, geneList1, geneList2);
 };
