6e876bb1352159b0afdf011bd6d3ce2630d72c14
hamjongseok <irang4605@naver.com>
2023-03-20 16:00:53 +0900

    [Feature] rechart 원형그래프 라이브러리 적용중

---
+++ src/components/morphCalc/morphCalcResult/CalPieChart.jsx
@@ -0,0 +1,63 @@
+import { PieChart, Pie, Legend, Tooltip, Cell } from 'recharts';
+
+const colors = [
+  '#0088FE',
+  '#00C49F',
+  '#FFBB28',
+  '#FF8042',
+  '#AF19FF',
+  '#FF1919',
+];
+
+const RADIAN = Math.PI / 180;
+
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+  return (
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline="central"
+    >
+      {`${percent * 100}%`}
+    </text>
+  );
+};
+
+export function CalPieChart(props) {
+  const { data } = props;
+
+  return (
+    <PieChart width={400} height={400}>
+      <Pie
+        data={data}
+        cx={300}
+        cy={200}
+        labelLine={false}
+        label={renderCustomizedLabel}
+        outerRadius={80}
+        fill="#8884d8"
+        dataKey="value"
+      >
+        {data.map((entry, index) => (
+          <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+        ))}
+      </Pie>
+      <Tooltip />
+      <Legend align="center" verticalAlign="bottom" />
+    </PieChart>
+  );
+}
