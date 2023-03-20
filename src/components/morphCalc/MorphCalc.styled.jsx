7811131eeb96d9fa46be0183e87af560cc4e5e0f
Juhee Noh <nohjuhee227@gmail.com>
2023-03-20 13:21:44 +0900

    [styles] 버튼 container margin-top 추가 #16

--- src/components/morphCalc/MorphCalc.styled.jsx
+++ src/components/morphCalc/MorphCalc.styled.jsx
@@ -5,7 +5,7 @@ export const DivMorphCalc = styled.div`
   width: 375px;
   border: 1px solid black;
   border-radius: 10px;
-  height: 100vh;
+  ${'' /* height: 100vh; */}
   padding-top: 30px;
   padding-left: 20px;
   padding-right: 20px;
@@ -20,6 +20,7 @@ export const MorphCalcBtnContainer = styled.div`
   display: flex;
   flex-flow: row nowrap;
   gap: 20px;
+  margin-top: 9px;
 `;
 export const BtnCalc = styled.button`
   width: 120px;
