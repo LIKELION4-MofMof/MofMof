00a151f1cedd8b6f7140f82dbaf12455a0941691
hamjongseok <irang4605@naver.com>
2023-03-23 23:52:56 +0900

    [Err] Search

--- src/components/morphCalc/morphInputContainer/MorphInputContainer.jsx
+++ src/components/morphCalc/morphInputContainer/MorphInputContainer.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
-import { ReactComponent as Search } from 'assets/icon/search.svg';
+import { ReactComponent as Search } from 'assets/icon/Search.svg';
 import MorphListDropBox from './MorphListDropBox';
 import {
   InputTitle,
