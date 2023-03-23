a82cf6563de3eda0b273656ab38d419a4c46f27b
Juhee Noh <nohjuhee227@gmail.com>
2023-03-23 14:27:22 +0900

    [bug] firebase 연동 에러 해결, 데이터 렌더링 성공 #32

--- src/components/morphInfo/MorphInfo.jsx
+++ src/components/morphInfo/MorphInfo.jsx
@@ -1,92 +1,35 @@
 import { Header } from 'components/common/Header/Header';
 import { Navigation } from 'components/common/Navigation/Navigation';
-import { db } from 'firebase/app';
+import { db } from 'firebase-db/app';
 import { collection, getDocs } from 'firebase/firestore';
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import FattailMorphListItem from './FattailMorphListItem';
 import { MorphInfoMain, MorphListUL } from './MorphInfo.styled';
 
-const fattailMorphList = [
-  {
-    id: 'ft1',
-    group: '단일',
-    name: '노말',
-    img: ['assets/fattail-morph-image/fattailNormal1.png'],
-    alt: ['첫번째 사진에 대한 설명'],
-    detail: '노말에 대한 설명입니다.',
-  },
-  {
-    id: 'ft2',
-    group: '2콤보',
-    name: '고스트',
-    img: [
-      'assets/fattail-morph-image/fattailGhost1.png',
-      'assets/fattail-morph-image/fattailGhost2.png',
-      'assets/fattail-morph-image/fattailGhost3.png',
-      'assets/fattail-morph-image/fattailGhost4.png',
-      'assets/fattail-morph-image/fattailGhost5.png',
-    ],
-    alt: [
-      '첫번째 사진 설명',
-      '두번째 사진 설명',
-      '세번째 사진 설명',
-      '네번째 사진 설명',
-      '다섯번째 사진 설명',
-    ],
-    detail:
-      '펫테일게코가 탈피 할 때의 색처럼 회색빛이라 고스트 라는 이름이 생겼다.',
-  },
-  {
-    id: 'ft3',
-    group: '단일',
-    name: '카라멜',
-    img: ['assets/fattail-morph-image/fattailNormal1.png'],
-    alt: ['첫번째 사진에 대한 설명'],
-    detail: '노말에 대한 설명입니다.',
-  },
-  {
-    id: 'ft4',
-    group: '2콤보',
-    name: '아멜라니스틱',
-    img: [
-      'assets/fattail-morph-image/fattailGhost1.png',
-      'assets/fattail-morph-image/fattailGhost2.png',
-      'assets/fattail-morph-image/fattailGhost3.png',
-      'assets/fattail-morph-image/fattailGhost4.png',
-      'assets/fattail-morph-image/fattailGhost5.png',
-    ],
-    alt: [
-      '첫번째 사진 설명',
-      '두번째 사진 설명',
-      '세번째 사진 설명',
-      '네번째 사진 설명',
-      '다섯번째 사진 설명',
-    ],
-    detail:
-      '펫테일게코가 탈피 할 때의 색처럼 회색빛이라 고스트 라는 이름이 생겼다.',
-  },
-];
-
-// 'posts' 컬렉션의 모든 문서들을 가져옴
 const MorphInfo = () => {
+  const [morphList, setMorphList] = useState([]);
   useLayoutEffect(() => {
     (async () => {
       const querySnapshot = await getDocs(
         collection(db, 'fat-tail-morph-list'),
       );
+      console.log(querySnapshot);
+      let docsData = [];
       querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
+        docsData.push({ id: doc.id, ...doc.data() });
       });
+      setMorphList(docsData);
     })();
   }, []);
+  console.log(morphList);
   return (
     <div className="App">
       <Header />
       <MorphInfoMain className="mainContainer">
         <section>
           <MorphListUL>
-            {fattailMorphList.map((morph) => (
+            {morphList.map((morph) => (
               <li key={morph.id}>
                 <Link to="/">
                   <FattailMorphListItem morph={morph} />
--- src/firebase-db/app.js
+++ src/firebase-db/app.js
@@ -21,4 +21,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
-console.log(db);
