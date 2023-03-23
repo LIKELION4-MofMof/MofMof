f877abd6f75bb2e67e5da4a2c4a3d6c7b37224f5
Juhee Noh <nohjuhee227@gmail.com>
2023-03-23 22:28:34 +0900

    [feat] firebase storage 이용하여 사진 데이터 불러오기 #37

--- src/components/morphInfo/FattailMorphListItem.jsx
+++ src/components/morphInfo/FattailMorphListItem.jsx
@@ -1,10 +1,9 @@
 import { MorphListItemFigure } from './MorphInfo.styled';
-import img1 from 'assets/fattailMorphImage/fattailNormal1.png';
 
 const FattailMorphListItem = ({ morph }) => {
   return (
     <MorphListItemFigure>
-      <img src={img1} alt={morph.alt[0]} />
+      <img src={morph.imgUrl[0]} alt={morph.alt[0]} />
       <figcaption>{morph.name}</figcaption>
     </MorphListItemFigure>
   );
