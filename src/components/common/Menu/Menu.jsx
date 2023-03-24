bf8a7c9c21af0c970d59b16dca4b627dbbf4c522
Juhee Noh <nohjuhee227@gmail.com>
2023-03-24 18:41:01 +0900

    [styles] menu 닫기 svg 색깔 변경

--- src/components/common/Menu/Menu.jsx
+++ src/components/common/Menu/Menu.jsx
@@ -1,16 +1,25 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from 'react';
 import Logo from 'components/common/Header/Logo';
 import MenuList from './MenuList';
-import { Greeting } from "components/common/Menu/Greeting";
-import { MenuWrapper, HeaderMenu, MenuButton, Bar, CloseBtn, MainMenuTitle, UserMenu, UserMenuList, GeckoMainMenuIcon, UserMenuIcon } from "./Menu.styled"
-import close from "assets/icon/close.svg";
-import customer from "assets/icon/customer.svg";
-import logOut from "assets/icon/logOut.svg";
-import setting from "assets/icon/setting.svg";
+import { Greeting } from 'components/common/Menu/Greeting';
+import {
+  MenuWrapper,
+  HeaderMenu,
+  MenuButton,
+  Bar,
+  CloseBtn,
+  MainMenuTitle,
+  UserMenu,
+  UserMenuList,
+  GeckoMainMenuIcon,
+  UserMenuIcon,
+} from './Menu.styled';
+import { ReactComponent as CloseIcon } from 'assets/icon/close.svg';
+import customer from 'assets/icon/customer.svg';
+import logOut from 'assets/icon/logOut.svg';
+import setting from 'assets/icon/setting.svg';
 
-
-
-const Menu = ({children, ...restProps}) => {
+const Menu = ({ children, ...restProps }) => {
   const [toggleMenu, setToggleMenu] = useState(false);
   const menuRef = useRef();
 
@@ -28,33 +37,45 @@ const Menu = ({children, ...restProps}) => {
     };
   });
 
-  const clickMenuOutside = e => {
-    if (CloseBtn && !menuRef.current.contains(e.target))
-    setToggleMenu();
-  }
-
+  const clickMenuOutside = (e) => {
+    if (CloseBtn && !menuRef.current.contains(e.target)) setToggleMenu();
+  };
 
   return (
     <MenuWrapper>
-      <MenuButton type="button" onClick={toggleChange} aria-label="메뉴">
-      </MenuButton>
-      <HeaderMenu toggleMenu={toggleMenu} ref={menuRef} >
+      <MenuButton
+        type="button"
+        onClick={toggleChange}
+        aria-label="메뉴"
+      ></MenuButton>
+      <HeaderMenu toggleMenu={toggleMenu} ref={menuRef}>
         <Logo fill="#ffff" />
-        <CloseBtn aria-label="닫기" onClick={() => setToggleMenu(false)}><img src={close} /></CloseBtn>
+        <CloseBtn aria-label="닫기" onClick={() => setToggleMenu(false)}>
+          <CloseIcon fill="#ffff" />
+        </CloseBtn>
         <Greeting />
-        <Bar/>
+        <Bar />
         <MenuList />
         <Bar />
         <div>
           <UserMenu>
-            <UserMenuList><UserMenuIcon src={customer} alt="고객지원 아이콘" /><MainMenuTitle>고객지원</MainMenuTitle></UserMenuList>
-            <UserMenuList><UserMenuIcon src={setting} alt="설정 아이콘" /><MainMenuTitle>설정</MainMenuTitle></UserMenuList>
-            <UserMenuList><UserMenuIcon src={logOut} alt="로그아웃 아이콘" /><MainMenuTitle>로그아웃</MainMenuTitle></UserMenuList>
+            <UserMenuList>
+              <UserMenuIcon src={customer} alt="고객지원 아이콘" />
+              <MainMenuTitle>고객지원</MainMenuTitle>
+            </UserMenuList>
+            <UserMenuList>
+              <UserMenuIcon src={setting} alt="설정 아이콘" />
+              <MainMenuTitle>설정</MainMenuTitle>
+            </UserMenuList>
+            <UserMenuList>
+              <UserMenuIcon src={logOut} alt="로그아웃 아이콘" />
+              <MainMenuTitle>로그아웃</MainMenuTitle>
+            </UserMenuList>
           </UserMenu>
         </div>
       </HeaderMenu>
     </MenuWrapper>
-  )
-}
+  );
+};
 
 export default Menu;
