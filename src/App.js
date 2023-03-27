f1f27119e7a401ffc610de6fd4ba2de0840f4404
SunheeJung <sunnnyj221@gmail.com>
2023-03-27 11:40:18 +0900

    [set] 환경설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Layout from './components/common/Layout/Layout';
 import Fattail from './components/main/Fattail';
 import Crested from './components/main/Crested';
 import Leopard from './components/main/Leopard';
+import Main from 'components/main/Main';
 
 
 import { Header } from './components/common/Header/Header';
 import { Navigation } from './components/common/Navigation/Navigation';
+
+
 
 
 function App() {
@@ -15,10 +18,10 @@ function App() {
     <div className="App">
       <Header/>
       <Navigation />
-
-      <Fattail />
+      <Main />
+      {/* <Fattail />
       <Crested />
-      <Leopard />
+      <Leopard /> */}
     </div>
 )}
 
