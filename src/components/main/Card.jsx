f6943a18e8fec9cbdb841604f9871b0c797430f3
SunheeJung <sunnnyj221@gmail.com>
2023-03-27 11:41:07 +0900

    [feat] 메인페이지 필터 구현

---
+++ src/components/main/Card.jsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import Img from '../../assets/image/lizard05.svg';
+import Star from '../../assets/icon/star.svg'
+import StarFill from '../../assets/icon/star-f.svg'
+import { Link } from 'react-router-dom';
+
+
+export default function Card({key, name, imgUrl,link}) {
+  return (
+    <Link to={link}>
+    <StyledCard>
+      <CardTitle key={key}>
+        <p>{name}</p>
+        <StyledButton></StyledButton>
+      </CardTitle>
+      <StyledImg src={require('../../assets/image/' + imgUrl + '.svg')} alt="" />
+    </StyledCard>
+    </Link>
+    
+  )
+}
+
+const StyledCard = styled.div`
+  width: 158px;
+  height: 240px;
+  background: #5F0BCE;
+  border-radius: 20px;
+  padding: 20px 10px;
+`
+const CardTitle = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+`
+const StyledImg = styled.img`
+  width: 141px;
+  margin-top: 20px;
+`
+const StyledButton = styled.button`
+  background: url(${Star}) no-repeat center;
+  width: 24px;
+  height: 24px;
+  border: none;
+`
---
+++ src/components/main/Lizardmainlist.jsx
@@ -0,0 +1,30 @@
+
+export const lizardcardlist = [
+  {
+    name : "펫테일게코",
+    imgUrl : "lizard05",
+    category : "category1",
+    link: "/fattail-home",
+  },
+  {
+    name : "레오파드게코",
+    imgUrl : "lizard02",
+    category : "category1"
+    ,
+    link: "/fattail-home",
+  },
+  {
+    name : "그레스티드게코",
+    imgUrl : "lizard03",
+    category : "category2"
+    ,
+    link: "/fattail-home",
+  },
+  {
+    name : "이구아나",
+    imgUrl : "lizard04",
+    category : "category1",
+    link: "/fattail-home",
+  },
+];
+
