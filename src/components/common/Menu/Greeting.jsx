6fe1e871916f4365c62c25274b7c4bfb571755db
sorinworld <jsorin1123@gmail.com>
2023-03-28 15:57:56 +0900

    [feat] 로그인 로그인 상태에 따른 ui 변경 #67

--- src/components/common/Menu/Greeting.jsx
+++ src/components/common/Menu/Greeting.jsx
@@ -1,35 +1,48 @@
 
 import { LoginRegister, LoginWrapper, UserProfile, LoginBtnWrapper, LoginBtn, LoginIndex, LoginBtnIcon } from "./Menu.styled"
 import { Link } from 'react-router-dom';
+import { auth } from '../../../firebase-db/auth-db/index';
+import { useState, useEffect } from 'react';
 
 
-function GuestGreeting(props) {
+
+
+function GuestGreeting() {
+
+  // console.log(auth);
   return (
-  <LoginRegister>
-    <UserProfile />
-    <LoginWrapper>
-    <Link to={"/login"}>
-      <LoginBtnWrapper>
-        <LoginBtn type="button">로그인 & 가입하기</LoginBtn>
-        <LoginBtnIcon/>
-    </LoginBtnWrapper>
-    </Link>
-      <LoginIndex>로그인하고 모프모프 서비스를 자유롭게 이용해보세요</LoginIndex>
-    </LoginWrapper>
-  </LoginRegister>
-  )
-}
+    <LoginRegister>
+      <UserProfile />
+      <LoginWrapper>
+      <Link to={"/login"}>
+        <LoginBtnWrapper>
+          <LoginBtn type="button">로그인 & 가입하기</LoginBtn>
+          <LoginBtnIcon/>
+      </LoginBtnWrapper>
+      </Link>
+        <LoginIndex>로그인하고 모프모프 서비스를 자유롭게 이용해보세요</LoginIndex>
+      </LoginWrapper>
+    </LoginRegister>
+    )
+  }
 
-function UserGreeting(props) {
+function UserGreeting() {
+  const user = auth.currentUser;
+  if (user !== null) {
+    const displayName = user.displayName;
+    const email = user.email;
+  }
   return(
     <LoginRegister>
       <UserProfile />
       <LoginWrapper>
         <LoginBtnWrapper>
-          <LoginBtn type="button">도마뱀귀엽조</LoginBtn>
-          <LoginBtnIcon></LoginBtnIcon>
+          <Link to={"/myPage"}>
+          <LoginBtn type="button">{user.displayName}</LoginBtn>
+          </Link>
+          <LoginBtnIcon></LoginBtnIcon>         
         </LoginBtnWrapper>
-        <LoginIndex>domabam@mofmof.com</LoginIndex>
+        <LoginIndex>{user.email}</LoginIndex>
       </LoginWrapper>
   </LoginRegister>
   )
@@ -38,7 +51,20 @@ function UserGreeting(props) {
 
 
 export function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
+  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      console.log(user);
+      if(user) {
+        //로그인
+        setIsLoggedIn(true);
+      } else {
+        //로그아웃
+        setIsLoggedIn(false);
+      }
+    });
+  }, []);
   return (
     <>
     { isLoggedIn ? (<UserGreeting />) : (<GuestGreeting />)}
